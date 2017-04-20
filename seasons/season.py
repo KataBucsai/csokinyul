ad6231b8fa93ebc5dfe1c947e082b67431690737
bekor <bekor@gmail.com>
2017-04-20 13:41:02 +0200

    season: added part field function for appending field properties

--- seasons/season.py
+++ seasons/season.py
@@ -28,20 +28,14 @@ def start_module(season):
     actualseason_file_name = current_file_path + "/actualseason.csv"
     actual_table = data_manager.get_table_from_file(file_name)
 
-    if not actual_table and season == "new": # first season here
+    if not actual_table and season == "new":  # first season here
         actual_table = new_season(actual_table)
     if season == "new":
         # future: export actual season to history and clear it 
         actual_table = new_season(actual_table)
+        data_manager.write_datatable_to_file("actualseason.csv", actual_table)
     if actual_table or season == "actual":
-        actual_table = actual_season()
-        
-
-    while True:
-        os.system("clear")
-        ui.print_menu("Selling", options, "Back to Main menu")
-
-        ui.get_inputs(["Press a key to continue"], "")
+        actual_table = actual_season(actual_table)
 
 
 # handle new season
@@ -50,14 +44,32 @@ def start_module(season):
 def new_season(season_table):
     field = []
     if not season:
-        ui.print_result("This is your first season")
+        ui.print_result("", "This is your first season")
         season_date = ui.get_inputs(["Year", "Month"], "What is the year and the month of your first season?")
         field.append("".join(date for date in season_date))
         field_size = ui.get_inputs(["Field Width (m)", "Field Length (m)"], "Insert your Farm size")
         field.append(",".join([int(field_size[0]) * int(field_size[1]), field_size[0], field_size[1]]))
-    ui.print_field()
-
+    ui.print_field(field, season_date)
     return field
+
+
+def actual_season(season_table):
+    while True:
+        field_plant = get_field_plant()
+        return 1
+
+
+def get_field_plant():
+    part_field = ui.get_inputs(["Field number", "Plant name"], "Select a field number and a plant")
+    if part_field == "1":
+        part_field.append("F2+F3")
+    elif part_field == "2:
+        part_field.append("F1+F4")
+    elif part_field == "3":
+        part_field.append("F1+F4")
+    else:
+        part_field.append("F2+F3")
+    return part_field
 
 
 # print the default table of records from the file
