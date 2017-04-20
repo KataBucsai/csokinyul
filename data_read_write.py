659360d5f2fcc3062cb20461efea6946daf86b3f
bekor <bekor@gmail.com>
2017-04-20 23:43:15 +0200

    fixed data write correctly writing data row by row, read is not duplicating file rout

--- data_read_write.py
+++ data_read_write.py
@@ -23,15 +23,16 @@ def get_datatable_from_file(file_name):
         return 0
 
 
-# write a @table into a file
+# write a @datatable into a file
 #
 # @file_name: string
-# @table: list of lists of strings
+# @datatable: list of lists of strings
 def write_datatable_to_file(file_name, datatable):
     try:
-        with open((main_path + '/' + file_name), 'w') as csvfile:
+        with open((main_path + '/' + file_name), 'w', newline="") as csvfile:
             datafile_writer = csv.writer(csvfile, delimiter=';', skipinitialspace=True, lineterminator='\n')
-            datafile_writer.writerow([line for line in datatable])
+            for row in datatable:
+                datafile_writer.writerow(row)
             print("write is done")
     except:
         print("Something")
