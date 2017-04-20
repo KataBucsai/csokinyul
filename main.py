55d990f0e115d3b00ce341bef91cf7f087ba4fa7
KataBucsai <kata.kovacs@gmail.com>
2017-04-20 14:20:13 +0200

    modified py file imports

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ from importlib.machinery import SourceFileLoader
 current_file_path = os.path.dirname(os.path.abspath(__file__))
 # User interface module
 ui = SourceFileLoader("ui", current_file_path + "/ui.py").load_module()
-# Fieldview module
-field = SourceFileLoader("field", current_file_path + "/fieldview/field.py").load_module()
-# Seasons module
-season = SourceFileLoader("season", current_file_path + "/seasons/season.py").load_module() 
-# General module
-general = SourceFileLoader("general", current_file_path + "/general.py").load_module() 
-# Data_read_write module
-data_read_write = SourceFileLoader("data_read_write", current_file_path + "/data_read_write.py").load_module()
 # Menu module
 menu = SourceFileLoader("menu", current_file_path + "/menu.py").load_module() 
 
