5631bf88e9cf962b0714d7c8c8ad637f7eaa8277
bekor <bekor@gmail.com>
2017-04-20 12:19:43 +0200

    season: new season added asking size and date now

--- seasons/season.py
+++ seasons/season.py
@@ -17,18 +17,25 @@ ui = SourceFileLoader("ui", current_file_path + "/../ui.py").load_module()
 # data manager module
 data_manager = SourceFileLoader("data_manager", current_file_path + "/../data_manager.py").load_module()
 # common module
-common = SourceFileLoader("common", current_file_path + "/../common.py").load_module()
+field = SourceFileLoader("common", current_file_path + "/../field/field.py").load_module()
 
 
 # start this module by a module menu like the main menu
 # user need to go back to the main menu from here
 # we need to reach the default and the special functions of this module from the module menu
 #
-def start_module():
+def start_module(season):
     actualseason_file_name = current_file_path + "/actualseason.csv"
     actual_table = data_manager.get_table_from_file(file_name)
 
-    if new_season(actual_table)
+    if not actual_table and season == "new": # first season here
+        actual_table = new_season(actual_table)
+    if season == "new":
+        # future: export actual season to history and clear it 
+        actual_table = new_season(actual_table)
+    if actual_table or season == "actual":
+        actual_table = actual_season()
+        
 
     while True:
         os.system("clear")
@@ -39,13 +46,18 @@ def start_module():
 
 # handle new season
 #
-# @season get 
-def new_season(season):
+# @season list of list
+def new_season(season_table):
+    field = []
     if not season:
         ui.print_result("This is your first season")
-    field_size = ui.get_inputs(["Field Width (m)", "Field Length (m)"], "Insert your Farm size")
-    
-    show_table()
+        season_date = ui.get_inputs(["Year", "Month"], "What is the year and the month of your first season?")
+        field.append("".join(date for date in season_date))
+        field_size = ui.get_inputs(["Field Width (m)", "Field Length (m)"], "Insert your Farm size")
+        field.append(",".join([int(field_size[0]) * int(field_size[1]), field_size[0], field_size[1]]))
+    ui.print_field()
+
+    return field
 
 
 # print the default table of records from the file
