02a8319131248d3ac859be8b77ee17665d422f41
bekor <bekor@gmail.com>
2017-04-20 16:38:11 +0200

    general:function for get longest element length from every column

--- general.py
+++ general.py
@@ -4,3 +4,12 @@ import sys
 from importlib.machinery import SourceFileLoader
 current_file_path = os.path.dirname(os.path.abspath(__file__))
 
+
+# return the max length of the longest element from every column
+#
+# @table: list of list
+def get_table_elements_length(table):
+    colum_max_lens = []
+    for i in range(len(table[0])):
+        colum_max_lens.append(len(max(table, key=lambda x: len(x[i]))[i]))
+    return colum_max_lens
