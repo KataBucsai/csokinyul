d110dc44494d44f791e8b68849712116cac2c148
bekor <bekor@gmail.com>
2017-04-21 00:55:23 +0200

    main: added menu option 3, show plants

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ ui = SourceFileLoader("ui", current_file_path + "/ui.py").load_module()
 menu = SourceFileLoader("menu", current_file_path + "/menu.py").load_module()
 # Seasons module
 season = SourceFileLoader("season", current_file_path + "/seasons/season.py").load_module()
+# Data_read_write module
+data_read_write = SourceFileLoader("data_read_write", current_file_path + "/data_read_write.py").load_module()
 
 
 def choose():
@@ -19,6 +21,10 @@ def choose():
         season.start_module("actual")
     if option == "2":
         season.start_module("new")
+    if option == "3":
+        plants_table = data_read_write.get_datatable_from_file("data/plants.csv")
+        ui.print_table(plants_table)
+        ui.get_inputs(["Press a key to continue"], "")
     if option == "0":
         sys.exit(0)
     else:
